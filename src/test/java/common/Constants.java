26ed1a2b8a58f3d84a25b00fcad31ff434abd1e8
onashase <onashase@gmail.com>
2026-03-09 17:45:16 +0530

    Constants.java commit

---
+++ src/test/java/common/Constants.java
@@ -0,0 +1,7 @@
+package common;
+
+public class Constants {
+    public static final String PRODUCT_URL = "https://www.ebay.com/itm/183393702861?_skw=wallet&itmmeta=01KK65CYFSXD9MPMSMJDYVVYZR&hash=item2ab31dd3cd:g:Kk8AAOSweEJgfhLv&itmprp=enc%3AAQALAAAA4GfYFPkwiKCW4ZNSs2u11xD9mwL637qrPnxdD3%2FbvL1%2FNGgkZYluF%2FEUv4k4XBojXmbYRQHaaEg%2Fjg3th%2BeQcl0SDCv7zWuwgfXVkkZNzYa1UhHAX7Ta5%2B5SX0HKd3753CibfN0gfhYlTq4IEZBwqYVyp%2FiumTxIwR2LjtDCMHPtfOkkoIxbTI%2FHn6XhCyCcwmKxifC3wfYx7vpQjn5gf%2Bs%2BE1wqcpVSEi1ky%2BBNF07tgq8huZxKV07kTNAiGbmWnRawMcM5qVG2clKIXLAXtmAVMTKHGqjHk1D6mvEL%2FbFq%7Ctkp%3ABk9SR5bos8WZZw";
+    public static final int VIEWPORT_HEIGHT = 921;
+    public static final int VIEWPORT_WIDTH = 1800;
+}
